d056efe5b6219af3c75d3fc98b1e22db2894f4b3
unknown <happykamogelo33@gmail.com>
2022-01-27 13:14:19 +0200

    added function login

--- home.js
+++ home.js
@@ -1,2 +1,8 @@
 
 
+const names = ["Happy","Nkosiphandule","Sithembiso","Andiswe","Sophie","Mduduzi","Wandile","Vusi","Precious","Selina","Kagisho","Mhlompheni","Lucky","Lebo","Khumbulani"]
+const age = ["30","43","19","34","18","20","25","27","54","22","33","39","44","28","40"]
+const display = document.getElementById("display");
+for (let i= 0; i < names.length; i++ ) {
+    display.innerHTML += "<li>" + names[i] + ", " + age[i] + "</li>";
+}
--- login.js
+++ login.js
@@ -3,16 +3,14 @@
 function login() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    console.log(password)
+
 
 
     if (email == "user@gmail.com" && password == "mypassword") {
         window.location.href = "home.html";
-        console.log('login')
+
     }
-    else {
-        alert("Invalid Email and Password");
-    }
+    
 
 
-}
+}  
